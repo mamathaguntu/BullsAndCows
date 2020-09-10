7f2f4b7545f57e95e8ac9517f83008d28efb2dbd
mamathaguntu <mamathaguntu@gmail.com>
2020-09-10 11:27:54 -0700

    Update BullsAndCows.java

--- Java/BullsAndCows.java
+++ Java/BullsAndCows.java
@@ -15,35 +15,26 @@ public class BullsAndCows {
         char[]  ch2 = guess.toCharArray();
         HashMap<Character, Integer> hmap = new HashMap<Character, Integer>();
         ArrayList<Character> num_cowsList = new ArrayList<Character>();
-        for (char c : ch1) {
-            // Add the values to the hashmap for further check
-           
-            if (hmap.size() != 0 && hmap.containsKey(c))
-            {
-                old_val = hmap.get(c);
-                new_val = old_val + 1;
-                hmap.replace(c, old_val, new_val);
-            }
-            else
-            {
-                hmap.put(c, 1);
-            }
-        }
+        
         //Check for Bulls in the guess code 
         for (int i = 0; i < ch2.length; i++)
         {
             if (ch1[i] == ch2[i])
             {
                 num_bulls++;
-                if (hmap.size() != 0 && hmap.containsKey(ch2[i]) && hmap.get(ch2[i]) > 0)
-                {
-                    old_val = hmap.get(ch2[i]);
-                    new_val = old_val - 1;
-                    hmap.replace(ch2[i], old_val, new_val);
-                }
             }
             else
             {
+                if (hmap.size() != 0 && hmap.containsKey(ch1[i]))
+                {
+                    old_val = hmap.get(ch1[i]);
+                    new_val = old_val + 1;
+                    hmap.replace(ch1[i], old_val, new_val);
+                }
+                else
+                {
+                    hmap.put(ch1[i], 1);
+                }
                 num_cowsList.add(ch2[i]);
             }
         }
